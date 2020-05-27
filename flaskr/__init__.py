4ff8167595c777a3788126170ee7cd3713678962
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-27 19:03:45 +0200

    Add database things

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -27,5 +28,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
 
     return app
--- flaskr/db.py
+++ flaskr/db.py
@@ -7,10 +7,8 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
+        g.db = sqlite3.connect(current_app.config['DATABASE'],
+                               detect_types=sqlite3.PARSE_DECLTYPES)
         g.db.row_factory = sqlite3.Row
 
     return g.db
@@ -22,7 +20,8 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-        def init_db():
+
+def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
@@ -35,3 +34,8 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
