eadc2fca718178b042173d503fe8b13a27ad6b2b
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-05-28 12:16:26 +0200

    Add comment section

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -41,6 +41,27 @@ def create():
     return render_template('blog/create.html')
 
 
+@bp.route('/<int:post_id>/comment', methods=('POST', ))
+@login_required
+def comment(post_id):
+    title = request.form['title']
+    body = request.form['body']
+    error = None
+
+    if not title:
+        error = 'Title is required.'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'INSERT INTO comments (title, body, author_id, post_id)'
+            ' VALUES (?, ?, ?, ?)', (title, body, g.user['id'], post_id))
+        db.commit()
+    return redirect(url_for('blog.view', id=post_id))
+
+
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -56,10 +77,19 @@ def get_post(id, check_author=True):
     return post
 
 
+def get_comments(post_id):
+    comments = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM comments p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.post_id = ?', (post_id, )).fetchall()
+    return comments
+
+
 @bp.route('/<int:id>/view')
 def view(id):
     post = get_post(id, False)
-    return render_template('blog/post.html', post=post)
+    comments = get_comments(id)
+    return render_template('blog/post.html', post=post, comments=comments)
 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
